4514172c559f439b581b6b2e25ae058314affdf6
langdawang678 <langdawang678@sina.com>
2019-06-10 08:47:22 +0800

    hpc review 4-2 demo

---
+++ Py_code/6Python接口测试框架实战与自动化进阶/DjangoInterface/base/demo4_2.py
@@ -0,0 +1,22 @@
+import  requests
+import json
+'''
+4-2 requests简单使用-post.mp4
+request.post访问login地址，
+login.html调用login方法（login方法在views.py中实现）
+即：Django_imooc\web\views.py的login方法
+'''
+data={
+    'username':'11',
+    'password':'22'
+}
+
+res =requests.post(url="http://127.0.0.1:8000/login/",data=data)
+print(res)
+print('res的类型是',type(res))
+
+print(res.text)
+print('res.text的类型是',type(res.text))
+
+print(res.json())
+print('res.json()的类型是',type(res.json()))
