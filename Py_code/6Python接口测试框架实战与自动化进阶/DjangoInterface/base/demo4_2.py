fc020614d612193d50c96642f467df8980063822
langdawang678 <langdawang678@sina.com>
2019-06-11 07:55:31 +0800

    hpc review 4-4demo

--- Py_code/6Python接口测试框架实战与自动化进阶/DjangoInterface/base/demo4_2.py
+++ Py_code/6Python接口测试框架实战与自动化进阶/DjangoInterface/base/demo4_2.py
@@ -13,10 +13,12 @@ data={
 
 res =requests.post(url="http://127.0.0.1:8000/login/",data=data)
 print(res)
-print('res的类型是',type(res))
+print('res的类型是',type(res),"\n")
 
+# str
 print(res.text)
-print('res.text的类型是',type(res.text))
+print('res.text的类型是',type(res.text),"\n")
 
+# dict
 print(res.json())
-print('res.json()的类型是',type(res.json()))
+print('res.json()的类型是',type(res.json()),"\n")
