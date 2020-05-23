c39ec88346266fe4036a98107351ed4cbb95462e
Administrator <langdawang678@sina.com>
2020-05-24 07:40:50 +0800

    完善算法的理解和注释

--- algorithm/binary_search.py
+++ algorithm/binary_search.py
@@ -1,28 +1,37 @@
 """
-##一个while True，直到匹配到时，return;或者次数超过长度一半时return
+二分：https://www.runoob.com/python3/python-binary-search.html
+有序序列，从中间开始找。
+一个while True（也可用递归）控制4种情况：
+a、当左侧index >右侧时，代表未找到
+b、当匹配到时（目标值=中间值），return;
+c、当目标值< 中间值时，则区间在左，右侧index = mid-1
+d、当目标值> 中间值时，则区间在右，左侧index = mid+1
 """
-def binary_search(arr, target):
-    right = len(arr)-1  # 最大索引=长度-1
+
+
+def binary_search(listarr, target):
+    right = len(listarr) - 1  # 最大索引=长度-1
     left = 0
     times = 1
     while True:
         mid = (left + right) // 2  # 中分（二分），取整
-        if left > right:    # 左边的索引大于右边的索引时，代表未找到target
+        if left > right:  # 左边的索引大于右边的索引时，代表未找到target
             print(target, "未找到")
             return
         else:
-            if arr[mid] > target:
+            if listarr[mid] > target:
                 right = mid - 1
-            elif arr[mid] == target:
+            elif listarr[mid] == target:
                 print("find target's index is", mid, ",cost %d times" % times)
                 return
             else:
                 left = mid + 1
             times += 1
 
+
 if __name__ == '__main__':
     # arr = [3,11,545,13,5,7,22,8]
-    arr =  [3, 5, 7, 8, 11, 13, 22, 545]
+    arr = [3, 5, 7, 8, 11, 13, 22, 545]
     binary_search(arr, 1)
     binary_search(arr, 3)
     binary_search(arr, 44)
---
+++ algorithm/select_sort.py
@@ -0,0 +1,14 @@
+"""
+选择排序：https://www.runoob.com/python3/python-selection-sort.html
+双for循环，外层控制轮数，内层控制每轮的最小值。
+每轮选择第1个数作为最小数，然后从左往右遍历出最小的。
+"""
+arr = [3.4, 12, 5, 77, 11, 3, 7, 8, 1]
+for i in range(len(arr)):
+    min_index = i  # 假定最左侧的是最小值
+    for j in range(i + 1, len(arr)):
+        if arr[j] < arr[min_index]:  # 当右侧比左侧还小时，把最小值下标给右侧，继续往右遍历
+            min_index = j  # 这两个都不用到i，而是“临时变量”arr[minindex]
+        #  下标给完后，也遍历结束，此时把最小值给到a[i]
+        arr[min_index], arr[i] = arr[i], arr[min_index]
+print(arr)
