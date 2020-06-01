51ade91c5bd722c0e9cd09364f1e34f2356825eb
chen <langdawang678@sina.com>
2020-06-01 22:14:15 +0800

    0601

--- algorithm/quick_sort.py
+++ algorithm/quick_sort.py
@@ -1,40 +1,33 @@
 '''
-https://zhuanlan.zhihu.com/p/63227573
-基准元素，一般来说选取有几种方法:
-取第一个元素
-取最后一个元素
-取第中位置元素间
-取第一个、最后一个、中间位置3者的中位数元素
-
-选择一个基准数，通过一趟排序后，
-将原序列分为两部分，使得前面的比后面的小，
-然后再依次对前后进行拆分进行快速排序，递归该过程，直到序列中所有记录均有序。
-
+https://blog.csdn.net/weixin_43250623/article/details/88931925
 '''
+def quick_sort(alist, start, end):
+    """快速排序"""
+    if start >= end:  # 递归的退出条件
+        return
+    mid = alist[start]  # 设定起始的基准元素
+    low = start  # low为序列左边在开始位置的由左向右移动的游标
+    high = end  # high为序列右边末尾位置的由右向左移动的游标
+    while low < high:
+        # 如果low与high未重合，high(右边)指向的元素大于等于基准元素，则high向左移动
+        while low < high and alist[high] >= mid:
+            high -= 1
+        alist[low] = alist[high]  # 走到此位置时high指向一个比基准元素小的元素,将high指向的元素放到low的位置上,此时high指向的位置空着,接下来移动low找到符合条件的元素放在此处
+        # 如果low与high未重合，low指向的元素比基准元素小，则low向右移动
+        while low < high and alist[low] < mid:
+            low += 1
+        alist[high] = alist[low]  # 此时low指向一个比基准元素大的元素,将low指向的元素放到high空着的位置上,此时low指向的位置空着,之后进行下一次循环,将high找到符合条件的元素填到此处
+
+    # 退出循环后，low与high重合，此时所指位置为基准元素的正确位置,左边的元素都比基准元素小,右边的元素都比基准元素大
+    alist[low] = mid  # 将基准元素放到该位置,
+    # 对基准元素左边的子序列进行快速排序
+    quick_sort(alist, start, low - 1)  # start :0  low -1 原基准元素靠左边一位
+    # 对基准元素右边的子序列进行快速排序
+    quick_sort(alist, low + 1, end)  # low+1 : 原基准元素靠右一位  end: 最后
 
 
-def quick_sort(lists, i, j):
-    if i >= j:
-        return list
-    pivot = lists[i]  # 1: 首先取序列第一个元素为基准元素pivot=R[low]
-    low = i
-    high = j
-    while i < j:
-        # 2：从后向前扫描，找小于等于pivot的数，如果找到，R[i]与R[j]交换，i++。
-        while i < j and lists[j] >= pivot:
-            j -= 1
-        lists[i] = lists[j]
-        # 3：从前往后扫描，找大于pivot的数，如果找到，R[i]与R[j]交换，j--
-        while i < j and lists[i] <= pivot:
-            i += 1
-        lists[j] = lists[i]
-    lists[j] = pivot
-    quick_sort(lists, low, i - 1)
-    quick_sort(lists, i + 1, high)
-    return lists
 
-
-if __name__ == "__main__":
-    lists = [30, 24, 5, 58, 18, 36, 12, 42, 39]
-    print("排序前的序列为：", lists)
-    print("\n排序后的序列为：", quick_sort(lists, 0, len(lists)-1))
+if __name__ == '__main__':
+    alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    quick_sort(alist, 0, len(alist) - 1)
+    print(alist)
