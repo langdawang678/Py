1d86d3c9aaf699f5ef9207067021464233a80681
Administrator <langdawang678@sina.com>
2020-08-09 13:23:08 +0800

    封装openpyxl， 1、对比write1-实例方法和write2-静态方法 2、对比openpyxl_handle2.py的不同封装方法

--- utils/openpyxl_handle.py
+++ utils/openpyxl_handle.py
@@ -1,3 +1,5 @@
+import time
+
 import openpyxl
 from openpyxl.worksheet.worksheet import Worksheet
 
@@ -10,20 +12,33 @@ from openpyxl.worksheet.worksheet import Worksheet
 
 
 class ExcelHandler:
-    def __init__(self, filepath):
+    def __init__(self, file):
         """初始化函数"""
-        self.filepath = filepath
+        self.file = file
         # 这里也可以把name放到init中，但是一次只能操作1个表单，先不用。
-        # 0.35 requests模块讲解和应用(一)-模块api---28分钟前
+        # 0.35 28分钟前
+        # 0.36 20分钟，self.wb = openpyxl.load_workbook(file),不推荐这么写，因为每次打开原来这个，下面函数修改会不生效。
 
     def open_sheet(self, name) -> Worksheet:
         """打开表单
         在函数后加-> 代表函数的返回类型 ，是一种函数注解
         作用：小技巧，解决 sheet = wb[name] 后，在pycharm中，无法sheet点操作
         """
-        wb = openpyxl.load_workbook(self.filepath)
+
+        '''
+        # 这里视频没有展示write1的实例方法（只有静态方法write2）
+        #!!!如下：wb和sheet不加self，会提示AttributeError: 'ExcelHandler' object has no attribute 'wb'
+        wb = openpyxl.load_workbook(self.file)
         sheet = wb[name]
+        wb.close()  # 一般读文件会自动关闭，写文件不关闭会造成数据混乱
         return sheet
+        '''
+
+        # 修改加入self后如下，且不影响get_*方法
+        self.wb = openpyxl.load_workbook(self.file)
+        self.sheet = self.wb[name]
+        self.wb.close()  # 一般读文件会自动关闭，写文件不关闭会造成数据混乱
+        return self.sheet
 
     def get_title(self, name):
         """获取首行，返回首行的list形式"""
@@ -60,14 +75,32 @@ class ExcelHandler:
             data.append(data_dict)  # 把每次的dict放到data中
         return data
 
-    def write(self):
+    def write1(self, sheet_name, row, col, value):
         """
         写入excel数据
         """
-        # 0.36，17分50秒
+        sheet = self.open_sheet(sheet_name)
+        sheet.cell(row, col).value = value
+        self.wb.save(self.file)
+        self.wb.close()
+
+    @staticmethod
+    def write2(file, sheet_name, row, col, data):
+        """
+        写入excel数据
+        """
+        wb = openpyxl.load_workbook(file)
+        sheet = wb[sheet_name]
+        sheet.cell(row, col).value = data
+        wb.save(file)  # 必须save，否则数据没保存
+        wb.close()
 
 
 # 用来先测试封装的内容：
 if __name__ == '__main__':
-    excel = ExcelHandler(r"excel.xlsx")
+    localtime = time.asctime(time.localtime(time.time()))
+
+    excel = ExcelHandler(r"excel.xlsx")  # r"excel.xlsx"
+    excel.write1("Sheet1", 4, 3, localtime)
+    # excel.write2(r"excel.xlsx", "Sheet1", 4, 3, localtime)
     print(excel.get_all("Sheet1"))
