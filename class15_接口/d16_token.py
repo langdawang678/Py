1abf3681d46080e52dd23988c9dc1311bdece616
Administrator <langdawang678@sina.com>
2020-08-09 22:13:22 +0800

    repush

--- class15_接口/d16_token.py
+++ class15_接口/d16_token.py
@@ -7,7 +7,26 @@ url = "http://120.78.128.25:8766/futureloan/member/login"
 headers = {"X-Lemonban-Media-Type": "lemonban.v2"}
 data = {"mobile_phone": "18111111111", "pwd": "12345678"}
 res = requests.post(url, json=data, headers=headers)
+print(type(res))  # <class 'requests.models.Response'>
+print(type(res.json()))  # <class 'dict'>
 print(res.json())
+
+'''
+{
+'code': 0, 
+'msg': 'OK', 
+'data': 
+    {
+    'id': 2049538, 'leave_amount': 502124.01, 'mobile_phone': '18111111111', 'reg_name': '小柠檬', 'reg_time': '2020-07-21 22:57:32.0', 'type': 1, 
+    'token_info': 
+        {
+        'token_type': 'Bearer', 'expires_in': '2020-08-09 21:33:52', 
+        'token': 'eyJhbGciOiJIUzUxMiJ9.eyJtZW1iZXJfaWQiOjIwNDk1MzgsImV4cCI6MTU5Njk4MDAzMn0.APdL9WJcyk1zL8g1F6G4vQ5KiNoYpOSk-9m4zz7kmco-vpyL0Dw3j_hIF6WX43yfYArY7qZSki847X6qxGXWqw'
+        }
+    }, 
+'copyright': 'Copyright 柠檬班 © 2017-2020 湖南省零檬信息技术有限公司 All Rights Reserved'
+}
+'''
 
 # 充值
 # token在上一个登录成功后获得，headers要哪些在接口文档中定义
---
+++ class_19_excel/common/requests_handler.py
@@ -0,0 +1,38 @@
+import requests
+
+"""
+封装requests方法
+"""
+
+# 基于HttpHandler.py（requests库的简单封装）
+class RequestsHandler:
+    def __init__(self):
+        self.session = requests.Session()
+
+    def visit(self, url, method, params=None, data=None, json=None, **kwargs):
+        """
+        根据入参method判断请求的方法
+        """
+        # if method.lower() == "get":
+        #     res = self.session.get(url, params=params, **kwargs)
+        # elif method.lower == "post":
+        #     res = self.session.post(url, params=params, data=data, json=json, **kwargs)
+        # else:
+
+        # sessions模块里的Session()类的request方法，可以自动判断method
+        '''
+            def request(self, method, url,
+            params=None, data=None, headers=None, cookies=None, files=None,
+            auth=None, timeout=None, allow_redirects=True, proxies=None,
+            hooks=None, stream=None, verify=None, cert=None, json=None):
+        '''
+        res = self.session.request(method, url, params=params, data=data, json=json, **kwargs)
+        try:
+            return res.json()
+        except ValueError:
+            print("result is not json ")
+
+
+if __name__ == '__main__':
+    httpHandle = RequestsHandler()
+    httpHandle.visit("https://wwww.baidu.com", "get", )  # result is not json
