790c7b03f5087262a822dc76d98d8eb1a462468c
chenxuanhuai <langdawang678@sina.com>
2020-10-28 20:49:03 +0800

    --

--- testing/test_PytestDemo.py
+++ testing/test_PytestDemo.py
@@ -17,7 +17,7 @@ def teardown_module():
 '''验证：函数级（setup_function/teardown_function）仅对函数用例生效。
 （即不在类中，每个函数执行一次）'''
 def test_function():
-    print("不在类中的测试方法")
+    print("测试用例，test_function，不在类中的测试方法")
 
 
 class TestPytest1(object):
@@ -25,11 +25,11 @@ class TestPytest1(object):
     # 2
     @classmethod
     def setup_class(cls):
-        print("\nsetup_class1，只执行一次")
+        print("\nsetup_class in TestPytest1，只执行一次")
 
     @classmethod
     def teardown_class(cls):
-        print("\nteardown_class1，只执行一次")
+        print("\nteardown_class in TestPytest1，只执行一次")
 
     # 3
     def setup(self):
@@ -51,34 +51,34 @@ class TestPytest1(object):
         print("\nteardown_function,仅对函数用例生效（即不在类中，每个函数执行一次）")
 
     # 被测试方法
-    def test_three(self):
-        print("test_three，测试用例")
+    def test_1(self):
+        print("测试用例，1")
 
-    def test_four(self):
-        print("test_four，测试用例")
+    def test_2(self):
+        print("测试用例，2")
 
 
 class TestPytest2(object):
 
     @classmethod
     def setup_class(cls):
-        print("\nsetup_class2，只执行一次")
+        print("\nsetup_class in TestPytest2，只执行一次")
 
     @classmethod
     def teardown_class(cls):
-        print("\nteardown_class2，只执行一次")
+        print("\nteardown_class in TestPytest2，只执行一次")
 
     def setup_method(self):
-        print("\nsetup_method2，每个测试方法都执行一次")
+        print("\nsetup_method in TestPytest2，每个测试方法都执行一次")
 
     def teardown_method(self):
-        print("teardown_method2，每个测试方法都执行一次")
+        print("teardown_method in TestPytest2，每个测试方法都执行一次")
 
-    def test_two(self):
-        print("test_two，测试用例")
+    def test_3(self):
+        print("测试用例，3")
 
-    def test_one(self):
-        print("test_one，测试用例")
+    def test_4(self):
+        print("测试用例，4")
 
 
 if __name__ == '__main__':
