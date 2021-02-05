585e41b660259c84a67913b9093c2b8a6a5e6d39
chenxuanhuai <langdawang678@sina.com>
2021-02-05 20:43:57 +0800

    py2.0 -斐波那契数列 -递归和非递归

---
+++ algorithm/斐波那契_递归和非递归.py
@@ -0,0 +1,62 @@
+"""先演一下，"斐波那契数列"是什么"""
+
+# 斐波那契数列，0开始
+n = 10
+if n == 1:
+    print(0)
+else:
+    a = 0
+    b = 1
+    count = 1
+    print(a, end=" ")
+    while count < n:
+        c = a + b
+        a = b
+        b = c
+        count = count + 1
+        print(a, end=" ")
+
+print()
+
+
+# 递归对时间和空间要求高，n越大要求的时间和空间越大，因此效率极低，不建议使用
+def ab(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    elif n >= 3:
+        return ab(n - 1) + ab(n - 2)
+
+
+print(ab(10))
+
+
+print("从1开始:  ",end='')
+n = 10
+if n == 1:
+    print(1)
+else:
+    a = 1
+    b = 2
+    count = 1
+    print(a, end=" ")
+    while count < n:
+        c = a + b
+        a = b
+        b = c
+        count = count + 1
+        print(a, end=" ")
+
+print()
+
+def ab(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n >= 3:
+        return ab(n - 1) + ab(n - 2)
+
+
+print(ab(10))
--- py2.0/stage2/3高阶函数/1.py
+++ py2.0/stage2/3高阶函数/1.py
@@ -1,4 +1,10 @@
-#!/usr/bin/python3
-# -*- coding: UTF-8 -*-
-# @date: 2021/2/2 7:49 下午
-# @author：langdawang678
+"""
+1）递归思想
+
+2）菲波那切数列
+
+3）纯函数的概念
+纯函数，函数副作用，引用透明
+https://www.cnblogs.com/snandy/archive/2011/08/14/2137898.html
+4）函数副作用
+5）lambda表达式"""
