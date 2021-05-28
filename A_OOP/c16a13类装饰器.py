ba238fbae6d3cfb4794d16267eaa2eb6646f9611
cxh <chenxuanhuai@sina.com>
2021-05-28 08:04:49 +0800

    0528 0804

--- A_OOP/c16a13类装饰器.py
+++ A_OOP/c16a13类装饰器.py
@@ -1,28 +1,47 @@
+"""类装饰器:
+通过类实现一个通用的装饰器
+即可装饰类,也可装饰函数
+"""
+
+
 # 类装饰器
-class MyCall(object):
-    def __init__(self, fun_cls):
-        # print("这个是init方法")
-        self.fun_cls = fun_cls
+class Decorator:
+    def __init__(self, func):
+        self.func = func
 
     def __call__(self, *args, **kwargs):
-        print("call方法")
-        return self.fun_cls(*args, **kwargs)
+        print("...装饰器里的功能1")
+        # self.func(*args, **kwargs)
+        return self.func(*args, **kwargs)
+        # print("...装饰器里的功能2")
 
 
-@MyCall
-def fun(a):  # fun = Mycall(fun)  此时的fun 是 Mycall的实例对象了,被调用时执行call方法
-    print("函数%s" % a)
+# 1/装饰函数
+@Decorator  # test01 = Decorator(test01)
+def test01(a):
+    print("fun1原来的函数", a)
+'''核心思路:
+@Decorator  # test01 = Decorator(test01) 
+上面的的Decorator(test01) 相当于实例化了一个对象.
+那调用test01()行数的时候,相当于对象直接加括号调用,所以类中要实现call方法
+
+'''
 
 
-@MyCall
-class My(object):  # My = Mycall(My)  此时的My 是 Mycall的实例对象了,被调用时执行call方法
+# 2/装饰类
+@Decorator
+class My:
     def __init__(self, name):
         self.name = name
+        print("类原来的功能")
 
 
-print(fun)  # <__main__.MyCall object at 0x0000022ECE480320> MyCall的实例对象
-fun(1)  # 实例被执行 执行的call方法，call方法里面执行了run()函数
+if __name__ == '__main__':
+    print(test01)  # <__main__.Decorator object at 0x0027F3A0>, 是 Decorator的实例化对象,同上面的思路
+    test01(1111)
 
-print(My)  # <__main__.MyCall object at 0x0000012B8FDB03C8> MyCall的实例对象
-m = My("111")  # MyCall的实例对象执行call方法 返回 My类的实例对象
-print(m)  # <__main__.My object at 0x0000012B8FDB0470> My的实例对象
+    print("-----------------")
+
+    print(My)  # <__main__.Decorator object at 0x0260E028> ,同上
+    m = My("111")
+    print(m)   # <__main__.My object at 0x011DE1F0>,需要在Decorator中call方法加return原函数
