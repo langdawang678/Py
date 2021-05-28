910b7138b022c1f4f74f895ffb3218db50468d93
chenxuanhuai <langdawang678@sina.com>
2021-05-28 14:13:33 +0800

    0528 1413

--- A_OOP/c16a04单例模式实现2_单例装饰器.py
+++ A_OOP/c16a04单例模式实现2_单例装饰器.py
@@ -1,39 +1,40 @@
-"""
-# todo
-
-1、https://www.cnblogs.com/jiangmingbai/p/10909449.html#%E6%96%B9%E5%BC%8F%E4%BA%8C%EF%BC%9A%E5%8D%95%E4%BE%8B%E8%A3%85%E9%A5%B0%E5%99%A8
-2、16的开头
-"""
+"""用装饰器实现单例模式
+可用于多个类"""
 
 
-# 装饰器单例模式
-def class_one_case(cls):
-    # 空字典储存 类 和 类实例（key:value）
-    __instance = {}
+def single(cls):
+    __instance = {}  # 空字典储存 类 和 类实例（key:value）
 
     def inner(*args, **kwargs):
         # 如果类不在字典中实例化对象储存，否者用字典中的对象
-        if cls not in __instance:
-            __instance[cls] = cls(*args, **kwargs)
+        if cls in __instance:
+            print(__instance)
             return __instance[cls]
         else:
+            __instance[cls] = cls(*args, **kwargs)
             return __instance[cls]
-
     return inner
 
 
-@class_one_case
-class TestClass(object):  # TestClass=class_one_case(TestClass) 调用的时候执行的装饰器内部inner方法，返回实例
-    name = "111"
+@single
+class Test:
+    def __init__(self, name):
+        self.name = name
+        print("...Test类初始化", self.name)
 
-    def run(self):
-        print(self.name)
+@single
+class Hello:
+    def __init__(self, name):
+        self.name = name
+        print("...Hello类初始化", self.name)
 
 
-t1 = TestClass()
-t2 = TestClass()
-print(id(t1))
-print(id(t2))
-t1.name = "222"
-# t2 就是t1  name 属性也都公共也变成"222"
-t2.run()  # 222 # 注释掉装饰器,则这里打印111
+t1 = Test("111")
+t2 = Test("222")
+print(t1.name)
+print(t2.name)
+print("------------------------------------------------")
+h1 = Hello("hhh")
+h2 = Hello("xxxxxxxxx")
+print(h1)
+print(h2)
