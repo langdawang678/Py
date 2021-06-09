2fe88c4750043a5f4b5283d78c62c2dd69be2b76
chenxuanhuai <langdawang678@sina.com>
2021-06-09 21:01:21 +0800

    0609 2101

--- A_OOP/@classmethod和@staticmethod.py
+++ A_OOP/@classmethod和@staticmethod.py
@@ -1,12 +1,22 @@
 
-"""
-写的比较好的文章:https://www.cnblogs.com/wcwnina/p/8644892.html
+"""写的比较好的文章:https://www.cnblogs.com/wcwnina/p/8644892.html"""
+'''
+1.实例方法只有实例调用,就是self
+2.类方法,可以类去调用,cls
+3.静态方法,在类里,但不接受类和实例的参数/方法,逻辑性代码可以类和实例调用,无self和cls
 
+
+静态方法是类中的函数，不需要实例。
+静态方法主要是用来存放逻辑性的代码，逻辑上属于类，但是和类本身没有关系，也就是说在静态方法中，不会涉及到类中的属性和方法的操作。
+可以理解为，静态方法是个独立的、单纯的函数，它仅仅托管于某个类的名称空间中，便于使用和维护。
+'''
+
+"""
 总结:
-                    实例的属性和方法    类的属性和方法     参数
-实例方法                   可访问        可访问          self
-@classmethod的方法         不可访问      可访问          cls
-@staticmethod的方法        不可访问      不可访问        空
+                    实例的属性和方法    类的属性和方法     参数        可被谁调用
+实例方法:                   可访问        可访问          self        仅实例调用
+@classmethod的方法:         不可访问      可访问          cls         类和实例
+@staticmethod的方法:        不可访问      不可访问        空           类和实例
 """
 
 def func():
@@ -44,4 +54,4 @@ t.classfunc()  # classmethod,类方法要加cls.方便类直接调用
 Test.classfunc()  # classmethod,类方法要加cls.方便类直接调用
 
 Test.staticfunc()  # staticmethod,静态方法,不要self和cls. 因为它和类功能相关,但是又和类的属性&实例没关系
-
+t.staticfunc()  # staticmethod,静态方法,不要self和cls. 因为它和类功能相关,但是又和类的属性&实例没关系
